63d175bd1dfa53529ea21fef50c1e20caf80c155
Maxime <maxbut@live.ca>
2021-10-10 23:34:25 -0400

    Added random collectable object rotation

--- Assets/Scripts/Rotater.cs
+++ Assets/Scripts/Rotater.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Rotater : MonoBehaviour
 {
 
+    private float RotationX;
+    private float RotationY;
+    private float RotationZ;
+
+    void Start()
+    {
+        RotationX = Random.Range(0, 45);
+        RotationY = Random.Range(0, 45);
+        RotationZ = Random.Range(0, 45);
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(RotationX, RotationY, RotationZ) * Time.deltaTime);
     }
 }
